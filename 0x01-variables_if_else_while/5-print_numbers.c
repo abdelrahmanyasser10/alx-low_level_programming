3d6debc4fe4f169bfbb1db2b04a93168514a3981
Abdelrahman Yasser <abdelrahmanyasser@DESKTOP-M5ATDM6.com>
2023-06-17 04:45:29 +0200

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all the digits
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	int i;
+
+	n = 10;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
