f288de87c50a480598ea8c74bd26d2c308664fbc
Abdelrahman Yasser <abdelrahmanyasser@DESKTOP-M5ATDM6.com>
2023-06-17 04:10:04 +0200

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  I sometimes suffer from insomnia.
+ * And when I can't fall asleep, I play what I call the alphabet game
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char alpha;
+
+        alpha = 'a';
+
+        while (alpha != 123)
+        {
+                putchar(alpha);
+                alpha++;
+        }
+	
+	alpha = 'A';
+
+	while (alpha != 91)
+	{
+		putchar(alpha);
+		alpha++;
+	}
+        putchar('\n');
+        return (0);
+}
