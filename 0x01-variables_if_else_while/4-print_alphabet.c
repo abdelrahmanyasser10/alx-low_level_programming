a93071421d966507698349a0c470dcca23222177
Abdelrahman Yasser <abdelrahmanyasser@DESKTOP-M5ATDM6.com>
2023-06-17 04:25:48 +0200

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha != 12)
+	{
+		if (aplha ==  'e' || alpha == 'q')
+			putchar(alpha);
+		alpha++;
+	}
+
+	putchar('\n');
+	return (0);
+}
