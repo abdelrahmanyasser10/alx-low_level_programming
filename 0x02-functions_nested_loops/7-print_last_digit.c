1bf071fa29a5a26f494dc7e035ba5fd8bb5a6ff7
Abdelrahman Yasser <abdelrahmanyasser@DESKTOP-M5ATDM6.com>
2023-06-25 10:19:21 +0200

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,6 @@
 
 int print_last_digit(int n)
 {
-	_putchar('0' + _abs(n % 10));
-	return (_abs(n % 10));
+	_putchar('0' + (n < 0 ? ((n * -1) % 10) : (n % 10)));
+	return (n < 0 ? ((n * -1) % 10) : (n % 10));
 }
