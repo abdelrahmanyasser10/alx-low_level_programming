99ce2b9245b1640d18f5517bc3262c5ae1041ca1
Abdelrahman Yasser <abdelrahmanyasser@DESKTOP-M5ATDM6.com>
2023-06-25 10:38:33 +0200

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,7 @@
  * Return: the last digit of a number
  */
 
-int print_last_digit(int n)
+int print_last_digit(long int n)
 {
 	_putchar('0' + (n < 0 ? ((n * -1) % 10) : (n % 10)));
 	return (n < 0 ? ((n * -1) % 10) : (n % 10));
