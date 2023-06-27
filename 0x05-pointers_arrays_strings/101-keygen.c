d25444ffdfd27a271285984e0e7cd5aaeefe73f6
abdelrahmanyasser10 <abdelrahmanyasser101112@gmail.com>
2023-06-28 02:16:52 +0300

    update

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+/**
+ * main - entry point 
+ *
+ * Description: a program that generates random valid
+ * passwords for the program 101-crackme.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	time_t t;
+	
+	srand((unsigned) time(&t));
+	printf("%d\n", rand());
+
+	return (0);
+}
