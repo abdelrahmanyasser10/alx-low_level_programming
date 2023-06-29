e7d6d938f08f2f2f0dfe7e56c25a3322fc669f48
abdelrahmanyasser10 <abdelrahmanyasser101112@gmail.com>
2023-06-29 18:50:18 +0300

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,18 +6,21 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: 15 if s1 > s2
- * -15 if s1 < s2
- *  0 if s1 == s2
+ * Return: 0 is s1 == s2
+ * other otherwise
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 == s2)
-		return (0);
-	else if (s1 > s2)
-		return (15);
-	else if (s1 < s2)
-		return (-15);
-	return (0);
+	int i = 0, m = 0;
+
+	while (m == 0)
+	{
+		if(*(s1 + i) == '\0' && *(s2 + i) == '\0')
+			break;
+		m = *(s1 + i) - *(s2 +i);
+		i++;
+	}
+
+	return (m);
 }
