8431b42990ad20db205cbf6faca7168cb391f77c
abdelrahmanyasser10 <abdelrahmanyasser101112@gmail.com>
2023-07-10 03:28:04 +0300

    update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,8 +11,8 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]__attribute__((unused)))
 {
-	printf("%d\n", argc + strlen(char **argv) - strlen(char **argv));
+	printf("%d\n", argc);
 	return (0);
 }
