9ca3bfbcef716c59398318043fb00d7059792826
abdelrahmanyasser10 <abdelrahmanyasser101112@gmail.com>
2023-07-18 02:37:31 +0300

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,12 +12,12 @@
  * Description: defined data type to store the information about a dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
