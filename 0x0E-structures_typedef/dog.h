3db2001d5d03911e4a9febc40bc936db9e0dcbb6
abdelrahmanyasser10 <abdelrahmanyasser101112@gmail.com>
2023-07-18 02:22:35 +0300

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
 
 /**
  * struct dog - a data type for dog
@@ -18,5 +19,6 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
