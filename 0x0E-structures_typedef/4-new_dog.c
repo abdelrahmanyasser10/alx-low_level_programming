37701e8d096cb831fe1a541cf499480a34c44f62
abdelrahmanyasser10 <abdelrahmanyasser101112@gmail.com>
2023-07-18 03:14:00 +0300

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,7 +15,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *nd = malloc(sizeof(*nd));
 
 	if (nd == NULL || name == NULL || owner == NULL)
+	{
+		free(nd);
 		return (NULL);
+	}
 
 	nd->name = malloc(sizeof(name) + 1);
 	nd->owner = malloc(sizeof(owner) + 1);
@@ -24,6 +27,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{	
 		free(name);
 		free(owner);
+		free(nd);
 		return (NULL);
 	}
 	strcpy(nd->name, name);
