57b488355cecc38aafb3b22e90aab6bac31b10ae
abdelrahmanyasser10 <abdelrahmanyasser101112@gmail.com>
2023-07-21 20:33:25 +0300

    update

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "calc.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: count of arguments
+ * @argv: array of arrguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, Result;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op = argv[2];
+
+	if (!IS_OPERATOR(op[0]) || strlen(op) != 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((op[0] == '/' || op[0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	Result = get_op_func(op)(a, b);
+
+	printf("%d\n", Result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "calc.h"
+
+/**
+ * op_add - sum funtcion
+ *
+ * @a: firtst operand
+ * @b: second operand
+ *
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction funtcion
+ *
+ * @a: firtst operand
+ * @b: second operand
+ *
+ * Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication funtcion
+ *
+ * @a: firtst operand
+ * @b: second operand
+ *
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division funtcion
+ *
+ * @a: firtst operand
+ * @b: second operand
+ *
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus funtcion
+ *
+ * @a: firtst operand
+ * @b: second operand
+ *
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
